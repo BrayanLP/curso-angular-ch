823b65343cafefe25ad9a874852530c5be0120fe
Brayan LP <brayan.laureano_consultor@mdp.com.pe>
2022-07-19 19:23:53 -0500

    :tada: subiend tarea 1

---
+++ tareas/ComponentesLaureano/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AsideComponent } from './aside/aside.component';
+import { ContentComponent } from './content/content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AsideComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
