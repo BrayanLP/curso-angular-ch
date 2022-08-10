f99d41eec47718f1145ef0482c57b722f7eac9dd
Brayan Lp <brayansystemlp@gmail.com>
2022-08-10 20:58:53 +0000

    :tada: actualizando cambios en form angular

--- tareas/FormLaureano/src/app/formulario/formulario.component.ts
+++ tareas/FormLaureano/src/app/formulario/formulario.component.ts
@@ -1,46 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.scss']
+  styleUrls: ['./formulario.component.scss'],
 })
 export class FormularioComponent implements OnInit {
-
   toppingList: string[] = ['Futbol', 'Basket', 'Natación', 'Tennis'];
 
-  showForm= true;
-  showResult= false;
-  form : FormGroup;
-  info: any = {}
+  showForm = true;
+  showResult = false;
+  form!: FormGroup;
+  info: any = {};
 
-  constructor(private fb: FormBuilder ) {
+  constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       nombre: ['', Validators.required],
       correo: ['', Validators.required],
       tipo: ['', Validators.required],
-      celular: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
-      descripcion: ['', Validators.required]
-    })
-   }
-
-  ngOnInit(): void {
+      celular: [
+        '',
+        [Validators.required, Validators.minLength(9), Validators.maxLength(9)],
+      ],
+      descripcion: ['', Validators.required],
+    });
   }
 
-  submit (form: FormGroup): void {
-    if(!form.invalid){
+  ngOnInit(): void {}
+
+  submit(): void {
+    if (!this.form.invalid) {
       console.log('perfecto');
-      this.info = form.value;
+      this.info = this.form.value;
       // this.showResult = true;
       // this.showForm = false;
-      form.reset();
+      this.form.reset();
       alert('Formulario enviado correctamente');
-    }
-    else{ 
-      console.log('en error:', form.value);
+    } else {
+      console.log('en error:', this.form.value);
     }
   }
-
 }
