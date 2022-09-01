a1f1aefd402737eedabc8198137c62d716cef795
Brayan Lp <brayansystemlp@gmail.com>
2022-09-01 17:33:51 +0000

    :tada: agregando routing

--- ProyectoFinal/src/app/estudiantes/estudiantes-routing.module.ts
+++ ProyectoFinal/src/app/estudiantes/estudiantes-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginGuard } from '../auth/guard/login.guard';
+import { CrearEstudianteComponent } from './components/crear-estudiante/crear-estudiante.component';
+import { EdicionEstudianteComponent } from './components/edicion-estudiante/edicion-estudiante.component';
+import { ListaEstudianteComponent } from './components/lista-estudiante/lista-estudiante.component';
+import { EstudiantesComponent } from './estudiantes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EstudiantesComponent,
+    canActivate: [LoginGuard] ,
+    children: [
+      {
+        path: "lista",
+        component: ListaEstudianteComponent,
+        canActivate: [LoginGuard] 
+      },
+      {
+        path: "edicion/:id",
+        component: EdicionEstudianteComponent,
+        canActivate: [LoginGuard] 
+      },
+      {
+        path: "crear",
+        component: CrearEstudianteComponent,
+        canActivate: [LoginGuard] 
+      },
+      
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
